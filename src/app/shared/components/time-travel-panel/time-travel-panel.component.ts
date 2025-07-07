1ff43ff3a87fffd8fae8ded4e519e7ba42876e2c
Juan Canelon <liscanojac@gmail.com>
2025-07-07 12:50:59 -0300

    feat: time-slider into the time-travel panel

--- src/app/shared/components/time-travel-panel/time-travel-panel.component.ts
+++ src/app/shared/components/time-travel-panel/time-travel-panel.component.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, model } from '@angular/core';
+import { TimeSliderComponent } from "../time-slider/time-slider.component";
+import { NumberInputComponent } from "../number-input/number-input.component";
+import { FormControl } from '@angular/forms';
+import { TimeTravelOptions } from '../../interfaces/time-travel-options';
 
 @Component({
   selector: 'app-time-travel-panel',
-  imports: [],
+  imports: [TimeSliderComponent, NumberInputComponent],
   templateUrl: './time-travel-panel.component.html',
   styleUrl: './time-travel-panel.component.css'
 })
 export class TimeTravelPanelComponent {
+
+  timeTravelDirection = model.required<boolean>();
+  inputNumber = input.required<FormControl<number | null>>();
+  inputLimits = input<TimeTravelOptions>();
+  
 
 }
