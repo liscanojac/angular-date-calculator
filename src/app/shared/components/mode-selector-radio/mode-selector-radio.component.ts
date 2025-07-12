c6440b309cc3d9128639c5227f0f4b95a3ea48e6
Juan Canelon <liscanojac@gmail.com>
2025-07-12 08:48:14 -0300

    feat: CalculatorMode type

--- src/app/shared/components/mode-selector-radio/mode-selector-radio.component.ts
+++ src/app/shared/components/mode-selector-radio/mode-selector-radio.component.ts
@@ -1,6 +1,6 @@
 import { Component, input, output } from '@angular/core';
 import { MatRadioModule, MatRadioChange } from '@angular/material/radio';
-import { RadioGroup } from '../../interfaces/radio-group';
+import { CalculatorMode, RadioGroup } from '../../interfaces/radio-group';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -11,8 +11,8 @@ import { FormsModule } from '@angular/forms';
 })
 export class ModeSelectorRadioComponent {
   options = input.required<RadioGroup>()
-  radioOptionChanged = output<string>()
   selectedOption = input.required<string>()
+  radioOptionChanged = output<CalculatorMode>()
 
   onSelectionChange(event: MatRadioChange): void {
     this.radioOptionChanged.emit(event.value)
